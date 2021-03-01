53e8109765f934d5bc065b2db72056615649b0e1
AnnaTimofejeva <timofejevaaa@gmail.com>
2021-03-01 20:05:52 +0200

    finished line

--- src/com/company/SomeClass.java
+++ src/com/company/SomeClass.java
@@ -3,6 +3,6 @@ package com.company;
 public class SomeClass {
     public static void main(String[] args) {
         System.out.println("Hello, again");
-        System.out.println("line1");
+        System.out.println("lin");
     }
 }
