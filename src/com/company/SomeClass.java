9be38328c1743a363b4066ee5864d816a8f31bf6
AnnaTimofejeva <timofejevaaa@gmail.com>
2021-03-01 19:56:30 +0200

    experiment

--- src/com/company/SomeClass.java
+++ src/com/company/SomeClass.java
@@ -2,6 +2,7 @@ package com.company;
 
 public class SomeClass {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("Hello, again");
+        System.out.println("line1");
     }
 }
