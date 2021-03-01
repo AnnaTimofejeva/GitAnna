32567a174167c1e22729c525003532076b657dff
AnnaTimofejeva <timofejevaaa@gmail.com>
2021-03-01 19:58:36 +0200

    changes in master

--- src/com/company/SomeClass.java
+++ src/com/company/SomeClass.java
@@ -3,5 +3,6 @@ package com.company;
 public class SomeClass {
     public static void main(String[] args) {
         System.out.println("Hello");
+        System.out.println("Experiment");
     }
 }
